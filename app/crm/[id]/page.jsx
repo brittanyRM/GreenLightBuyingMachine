5064d1b4a54e6c9822ec096ed65e7152fff1670d
Brittany-Barnabas Collective <Brittanycma@gmail.com>
2026-09-02 09:52:27 -0700

    Delete app directory

--- app/crm/[id]/page.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import ContactDetail from "../../../components/ContactDetail";
-
-export default function ContactPage({ params }) {
-  return <ContactDetail contactId={params.id} />;
-}
